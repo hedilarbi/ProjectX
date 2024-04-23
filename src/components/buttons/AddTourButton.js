849a6129a86230bb12d03c38ecaec3d1bb0dc707
hedilarbi <hedi.larbi@esprit.tn>
2024-04-23 12:30:52 +0100

    agency module initialisation

---
+++ src/components/buttons/AddTourButton.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const AddTourButton = () => {
+  return (
+    <TouchableOpacity className="bg-green-500 mt-5">
+      <Text>AddTourButton</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default AddTourButton;
--- src/screens/agencyScreens/AgencyExploreScreen.js
+++ src/screens/agencyScreens/AgencyExploreScreen.js
@@ -1,14 +1,13 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import AddTourButton from "../../components/buttons/AddTourButton";
 
 const AgencyExploreScreen = () => {
   const inset = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: inset.top }}>
-      <TouchableOpacity className="bg-green-500 mt-5">
-        <Text>AddTourButton</Text>
-      </TouchableOpacity>
+      <AddTourButton />
     </View>
   );
 };
